6e92219a8052e8377a751b900f9407f16c5558f2
Andrea Schiavini <andrea.schiavini@gmail.com>
2012-01-10 11:47:50 +0100

    Server mode working with sleeps, added output files

--- build.rb
+++ build.rb
@@ -1,14 +1,19 @@
+#!/usr/bin/env ruby
+
 def remote_cmd command
-  system "mvim -f -n --noplugin -u vimrc --servername \"VIMCOLORS\" --remote-send \":#{command} <CR>\""
+  system "mvim -f -n --noplugin -U vimrc --servername \"VIMCOLORS\" --remote-send \":#{command} <CR>\""
 end
 
 vim_colorschemes = Dir.entries(File.join(ENV['HOME'], '.vim', 'colors')).map { |c| c.gsub("\.vim", "") }.reject { |e| e == "." || e == ".." }
-#system "mvim -f -n --noplugin -u vimrc --servername \"VIMCOLORS\" samples/ruby.rb"
-vim_colorschemes.each do |vim_colorscheme|
+system "mvim -f -n --noplugin -U vimrc --servername \"VIMCOLORS\" samples/ruby.rb &"
+sleep 2
+vim_colorschemes.each_with_index do |vim_colorscheme, index|
   remote_cmd "colorscheme #{vim_colorscheme}"
   remote_cmd "TOhtml"
   remote_cmd "w! output/#{vim_colorscheme}.html"
   remote_cmd "bd!"
-  #command = "mvim -f -n --noplugin -u vimrc samples/ruby.rb -c \"colorscheme #{vim_colorscheme}\" -c \"TOhtml\" -c \"w\! output/#{vim_colorscheme}.html\" "
+  puts "Colorscheme #{vim_colorscheme} done (#{index}/#{vim_colorschemes.size})"
+  sleep 1
 end
+remote_cmd "qall!"
 
