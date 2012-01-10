e3696162adedcd8598d956e46a6340e2c16e8ea0
Andrea Schiavini <andrea.schiavini@gmail.com>
2012-01-10 10:09:50 +0100

    Working, but slow

--- build.rb
+++ build.rb
@@ -1,1 +1,5 @@
-mvim -f -n --noplugin -u vimrc samples/ruby.rb -c "colorscheme github" -c "TOhtml" -c "w\! pippo.html" -c "qa\!"
+vim_colorschemes = Dir.entries(File.join(ENV['HOME'], '.vim', 'colors')).map { |c| c.gsub("\.vim", "") }
+vim_colorschemes.each do |vim_colorscheme|
+  command = "mvim -f -n --noplugin -u vimrc samples/ruby.rb -c \"colorscheme #{vim_colorscheme}\" -c \"TOhtml\" -c \"w\! output/#{vim_colorscheme}.html\" -c \"qa\!\""
+  system(command)
+end
