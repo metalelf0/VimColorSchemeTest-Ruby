cc09166ff83e56340a3c64bb2ec2473182a93e87
Andrea Schiavini <andrea.schiavini@gmail.com>
2012-01-10 09:56:27 +0100

    Added vimrc

--- build.rb
+++ build.rb
@@ -1,1 +1,1 @@
-mvim -f -n --noplugin -u thisvimrc2 samples/ruby.rb -c "colorscheme github" -c "TOhtml" -c "w\! pippo.html" -c "qa\!"
+mvim -f -n --noplugin -u vimrc samples/ruby.rb -c "colorscheme github" -c "TOhtml" -c "w\! pippo.html" -c "qa\!"
