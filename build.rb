0079739c0d888bfa5bcbfd0c22be28bfc9f61eda
Andrea Schiavini <andrea.schiavini@gmail.com>
2012-01-10 11:28:28 +0100

    Server mode (almost) working

--- build.rb
+++ build.rb
@@ -1,5 +1,14 @@
-vim_colorschemes = Dir.entries(File.join(ENV['HOME'], '.vim', 'colors')).map { |c| c.gsub("\.vim", "") }
-vim_colorschemes.each do |vim_colorscheme|
-  command = "mvim -f -n --noplugin -u vimrc samples/ruby.rb -c \"colorscheme #{vim_colorscheme}\" -c \"TOhtml\" -c \"w\! output/#{vim_colorscheme}.html\" -c \"qa\!\""
-  system(command)
+def remote_cmd command
+  system "mvim -f -n --noplugin -u vimrc --servername \"VIMCOLORS\" --remote-send \":#{command} <CR>\""
 end
+
+vim_colorschemes = Dir.entries(File.join(ENV['HOME'], '.vim', 'colors')).map { |c| c.gsub("\.vim", "") }.reject { |e| e == "." || e == ".." }
+#system "mvim -f -n --noplugin -u vimrc --servername \"VIMCOLORS\" samples/ruby.rb"
+vim_colorschemes.each do |vim_colorscheme|
+  remote_cmd "colorscheme #{vim_colorscheme}"
+  remote_cmd "TOhtml"
+  remote_cmd "w! output/#{vim_colorscheme}.html"
+  remote_cmd "bd!"
+  #command = "mvim -f -n --noplugin -u vimrc samples/ruby.rb -c \"colorscheme #{vim_colorscheme}\" -c \"TOhtml\" -c \"w\! output/#{vim_colorscheme}.html\" "
+end
+
