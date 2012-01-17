ebfc96c15f35344051659125a77b64d7d43dbfb5
Andrea Schiavini <andrea.schiavini@gmail.com>
2012-01-17 15:21:52 +0100

    Fixed a bug in file writer

--- lib/file_writer.rb
+++ lib/file_writer.rb
@@ -1,11 +1,11 @@
 class FileWriter
 
 	def initialize target_file
-		file = File.open(target_file, "w")
+		@file = File.open(target_file, "w")
 	end
 
 	def write content
-		file.puts content
+		@file.puts content
 	end
 
 end
